d5150d492b33dd7751ba9b76fae7593e0cddb2ce
Ville <ville.kaikk@gmail.com>
2025-12-20 14:04:37 +0200

    Abstact device settings

--- ElgatoLightControl/Models/Keylight/KeylightSettings.cs
+++ ElgatoLightControl/Models/Keylight/KeylightSettings.cs
@@ -1,7 +1,7 @@
 namespace ElgatoLightControl.Models;
 
 
-public record KeylightSettings(int Brightness, int Temperature, bool On)
+public record KeylightSettings(int Brightness, int Temperature, bool On) : IElgatoDeviceSettings
 {
     public static KeylightSettings None => new KeylightSettings(0, 0, false);
 }
