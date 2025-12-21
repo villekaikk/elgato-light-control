e62b02d40bb7c9d85764c619ae01cee97829b928
Ville <ville.kaikk@gmail.com>
2025-12-21 19:36:17 +0200

    Implement basic KeyLightSettings view

--- ElgatoLightControl/ViewModels/Views/KeylightSettingsViewModel.cs
+++ ElgatoLightControl/ViewModels/Views/KeylightSettingsViewModel.cs
@@ -1,23 +1,69 @@
 using System;
 using System.Threading.Tasks;
+using ElgatoLightControl.Models.Keylight;
+using ElgatoLightControl.Services.Controllers;
 using ElgatoLightControl.ViewModels.Models;
 using ReactiveUI;
 
 namespace ElgatoLightControl.ViewModels.Views;
 
-public class KeylightSettingsViewModel :  ReactiveObject, IDeviceSettingsViewModel
+public class KeylightSettingsViewModel : ReactiveObject, IDeviceSettingsViewModel
 {
+    // private readonly KeylightController _ctrl = ctrl;
+
     public KeylightSettingsViewModel()
     {
-        
+        DeviceName = "Keylight Air";
+        FirmwareVersion = "1.0.0";
+        Brightness = 40;
+        Temperature = 1000;
+        On = true;
     }
 
     public async Task DisplayDevice(ElgatoDeviceViewModel? device)
     {
-        await Task.Delay(1);
         if (device is null)
             return;
         
-        Console.WriteLine($"Keylight Device Selected Callback - {device.DisplayName}");
+        var settings = (KeylightSettings)device.Settings;
+        DeviceName = device.DisplayName;
+        FirmwareVersion = device.FirmwareVersion;
+        Brightness = settings.Brightness;
+        Temperature = settings.Temperature;
+        On =  settings.On;
+        await Task.FromResult(0);
     }
+
+    public string DeviceName
+    {
+        get => field;
+        set => this.RaiseAndSetIfChanged(ref field, value);
+    }
+    
+    public string FirmwareVersion
+    {
+        get => field;
+        set =>  this.RaiseAndSetIfChanged(ref field, value);
+    }
+
+    public int Brightness
+    {
+        get => field;
+        set => this.RaiseAndSetIfChanged(ref field, value);
+    } = 0;
+
+    public int Temperature
+    {
+        get => field;
+        set => this.RaiseAndSetIfChanged(ref field, value);
+    } = 0;
+
+    public bool On
+    {
+        get => field;
+        set => this.RaiseAndSetIfChanged(ref field, value);
+    } = false;
+
+    public int MaxTemp => 7000;
+    public int MinTemp => 2900;
 }
